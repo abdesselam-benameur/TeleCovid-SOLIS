2b90a66f7c865fa51bd2bed6293bb4d284ced5ac
hamza <h.korbaa@esi-sba.dz>
2021-07-03 11:14:28 +0100

    modify message.dart , messageModel.dart

--- lib/Messeges/message.dart
+++ lib/Messeges/message.dart
@@ -17,11 +17,41 @@ class _ChatPageState extends State<ChatPage> {
   final primary = Color.fromARGB(255, 71, 63, 151);
   final secondary = Color.fromARGB(100, 77, 121, 255);
   final third = Color.fromARGB(255, 255, 77, 88);
+  final TextEditingController _text = new TextEditingController();
+  ScrollController scrollController = ScrollController();
 
-  _ChatPageState({Key? key, required this.index, required this.user}) {}
+  _ChatPageState({Key? key, required this.index, required this.user});
+
   @override
   Widget build(BuildContext context) {
     var messages = Users().getUser(index).messages;
+    messages.add(ChatMessage(
+        messageContent: "hi every one",
+        messageType: "Sender",
+        time: TimeOfDay.now().toString()));
+    callback() {
+      setState(() {
+        _text.addListener(() {
+          if (_text.text.isEmpty) {
+          } else {
+            setState(() {
+              ChatMessage value = ChatMessage(
+                  messageContent: _text.text,
+                  messageType: "Sender",
+                  time: TimeOfDay.now().toString());
+              messages.add(value);
+              //_text.clear();
+              scrollController.animateTo(
+                scrollController.position.maxScrollExtent,
+                curve: Curves.easeOut,
+                duration: const Duration(milliseconds: 300),
+              );
+            });
+          }
+        });
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -86,12 +116,64 @@ class _ChatPageState extends State<ChatPage> {
                 height: MediaQuery.of(context).size.height - 150,
                 width: double.infinity,
                 child: ListView.builder(
+                    controller: scrollController,
                     itemCount: messages.length,
                     shrinkWrap: true,
                     padding: EdgeInsets.only(top: 10, bottom: 10),
                     physics: NeverScrollableScrollPhysics(),
                     itemBuilder: (context, index) {
-                      return Container(
+                      return
+                          // Column(children: [
+                          Container(
+                              padding: EdgeInsets.only(
+                                  left: 14, right: 14, top: 10, bottom: 10),
+                              child: Align(
+                                alignment:
+                                    (messages[index].messageType == "receiver"
+                                        ? Alignment.topLeft
+                                        : Alignment.topRight),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: (messages[index].messageType ==
+                                            "receiver"
+                                        ? Colors.grey.shade200
+                                        : secondary),
+                                  ),
+                                  padding: EdgeInsets.all(16),
+                                  child: Text(
+                                    messages[index].messageContent,
+                                    style: TextStyle(fontSize: 15),
+                                  ),
+                                ),
+                              ));
+                      /*  newMessage
+                              ? Container(
+                                  padding: EdgeInsets.only(
+                                      left: 14, right: 14, top: 10, bottom: 10),
+                                  child: Align(
+                                    alignment: (messages[index].messageType ==
+                                            "receiver"
+                                        ? Alignment.topLeft
+                                        : Alignment.topRight),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: (messages[index].messageType ==
+                                                "receiver"
+                                            ? Colors.grey.shade200
+                                            : secondary),
+                                      ),
+                                      padding: EdgeInsets.all(16),
+                                      child: Text(
+                                        messages[index].messageContent,
+                                        style: TextStyle(fontSize: 15),
+                                      ),
+                                    ),
+                                  ))
+                              : Container(),*/
+                      // ]);
+                      /*Container(
                           padding: EdgeInsets.only(
                               left: 14, right: 14, top: 10, bottom: 10),
                           child: Align(
@@ -113,7 +195,7 @@ class _ChatPageState extends State<ChatPage> {
                                 style: TextStyle(fontSize: 15),
                               ),
                             ),
-                          ));
+                          ));*/
                     }),
               ),
             ),
@@ -148,17 +230,18 @@ class _ChatPageState extends State<ChatPage> {
                     ),
                     Expanded(
                       child: TextField(
-                        decoration: InputDecoration(
-                            hintText: "Écrire un message ...",
-                            hintStyle: TextStyle(color: Colors.black54),
-                            border: InputBorder.none),
-                      ),
+                          controller: _text,
+                          onSubmitted: (value) => callback(),
+                          decoration: InputDecoration(
+                              hintText: "Écrire un message ...",
+                              hintStyle: TextStyle(color: Colors.black54),
+                              border: InputBorder.none)),
                     ),
                     SizedBox(
                       width: 15,
                     ),
                     FloatingActionButton(
-                      onPressed: () {},
+                      onPressed: callback,
                       child: Icon(
                         Icons.send,
                         color: Colors.white,
@@ -174,101 +257,6 @@ class _ChatPageState extends State<ChatPage> {
           ],
         ),
       ),
-
-      /* body: Stack(
-        children: <Widget>[
-          SingleChildScrollView(
-            child: Container(
-              padding: EdgeInsets.only(top: 145, right: 20, left: 20),
-              height: MediaQuery.of(context).size.height,
-              width: double.infinity,
-              child: ListView.builder(
-                itemCount: messages.length,
-                shrinkWrap: true,
-                padding: EdgeInsets.only(top: 10, bottom: 10),
-                physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.only(
-                        left: 14, right: 14, top: 10, bottom: 10),
-                    child: Align(
-                      alignment: (messages[index].messageType == "receiver"
-                          ? Alignment.topLeft
-                          : Alignment.topRight),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: (messages[index].messageType == "receiver"
-                              ? Colors.grey.shade200
-                              : Colors.blue[200]),
-                        ),
-                        padding: EdgeInsets.all(16),
-                        child: Text(
-                          messages[index].messageContent,
-                          style: TextStyle(fontSize: 15),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-          /*  Align(
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
-              height: 60,
-              width: double.infinity,
-              color: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlue,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: "Write message...",
-                          hintStyle: TextStyle(color: Colors.black54),
-                          border: InputBorder.none),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.send,
-                      color: Colors.white,
-                      size: 18,
-                    ),
-                    backgroundColor: Colors.blue,
-                    elevation: 0,
-                  ),
-                ],
-              ),
-            ),
-          ),*/
-        ],
-      ),*/
     );
   }
 }
